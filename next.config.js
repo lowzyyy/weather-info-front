be075f944630b49938bbdf1f72e8674cc40d622e
lowzyyy <peratheking@aol.com>
2023-02-05 20:45:52 +0100

    added priority for some images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["https://s.w-x.co/"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.hidmet.gov.rs",
+        port: "",
+        pathname: "/data/**",
+      },
+    ],
   },
 };
 
--- src/components/Radar/ImagesList.js
+++ src/components/Radar/ImagesList.js
@@ -1,17 +1,22 @@
 import React from "react";
+import Image from "next/image";
 
 function ImagesList(props) {
   return (
     <>
       {props.links.map((l, i) => {
         return (
-          <img
+          <Image
             key={i}
             className={`${props.selectedTime === i ? "inline" : "hidden"} ${
               props.animation
             } rounded-sm`}
             src={`${l.link}`}
-          ></img>
+            alt="Radar image"
+            width={765}
+            height={565}
+            priority={i < 4 ? true : false}
+          ></Image>
         );
       })}
     </>
