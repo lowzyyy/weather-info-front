aa06642c21f3959a84e126ee239d950cc7b77b64
lowzyyy <peratheking@aol.com>
2023-02-10 18:25:36 +0100

    fixed webp problem (animations dont work on ios 14)

--- src/components/Radar/ImagesList.js
+++ src/components/Radar/ImagesList.js
@@ -5,24 +5,14 @@ function ImagesList(props) {
     <>
       {props.links.map((l, i) => {
         return (
-          // <img
-          //   key={i}
-          //   className={`absolute rounded-sm ${
-          //     props.selectedTime === i
-          //       ? `z-10 opacity-100 ${props.animation}`
-          //       : "z-0 opacity-0"
-          //   }`}
-          //   src={`${l.link}`}
-          //   alt="Radar image"
-          // />
-          <picture
+          <img
+            key={i}
             className={`absolute rounded-sm ${
-              props.selectedTime === i ? `z-10 opacity-100` : "z-0 opacity-0"
+              props.selectedTime === i ? `z-10 opacity-100 ` : "z-0 opacity-0"
             }`}
-          >
-            <source srcSet={`${l.link}`} />
-            <img src="../../../public/radarska_fallback.png" />
-          </picture>
+            src={`${l.link}`}
+            alt="Radar image"
+          />
         );
       })}
     </>
