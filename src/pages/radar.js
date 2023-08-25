66769cf099675843c7c27a638cb8c9d5476d36d3
lowzyyy <peratheking@aol.com>
2023-08-25 18:54:28 +0200

    removed infrared until i find the alternative

--- src/pages/radar.js
+++ src/pages/radar.js
@@ -9,13 +9,16 @@ const Radar = ({ url }) => {
     <>
       <Head>
         <title>Smederevo weather - radar</title>
-        <meta name="description" content="Meteoroloske informacije o gradu Smederevu" />
+        <meta
+          name="description"
+          content="Meteoroloske informacije o gradu Smederevu"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <UrlContext.Provider value={url}>
         <RadarCard></RadarCard>
-        <InfraredCard></InfraredCard>
+        {/* <InfraredCard></InfraredCard> */}
       </UrlContext.Provider>
     </>
   );
